5515675f419b17cb8ef6f93913a54fbc85917691
Tiago <tiago@softrom.com.br>
2021-02-23 11:42:10 -0300

    atualização das aulas

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,24 @@
+function load() {
+    alert("Página Carregada");
+}
+
 function clicou() {
     document.getElementById("agradecimento").innerHTML = "<br>Valeu por <b>Clicar no meu teste!!!</b>";
 }
 
 function redirecinar() {
     window.open("https://tiagojunio.com.br");
-    window.location.href = "https://tiagojunio.com.br";
+    // window.location.href = "https://tiagojunio.com.br";
 }
 
-function buttontrocar() {
-    window.open("https://tiagojunio.com.br");
-    document.getElementById("buttonmove").innerHTML = "indo para o site";
+function buttontrocar(elemento) {
+    elemento.innerHTML = "Indo para o Site";
+    // document.getElementById("buttonmove").innerHTML = "indo para o site";
 }
 
-function buttonvoltar() {
-    document.getElementById("buttonmove").innerHTML = "Clique Aqui";
+function buttonvoltar(elemento) {
+    elemento.innerHTML = "Clique aqui";
+    // document.getElementById("buttonmove").innerHTML = "Clique Aqui";
 }
 
 function trocar() {
@@ -22,4 +27,9 @@ function trocar() {
 
 function voltar() {
     document.getElementById("mousemove").innerHTML = "Passe o Mouse Aqui";
+}
+
+function funcaoChange(elemento) {
+    console.log(elemento.value);
+
 }
